113f0b47a61172d5a004def8ce8c31433d3c3ca6
elkin1704 <60784779+elkin1704@users.noreply.github.com>
2022-01-21 08:38:16 -0500

    Update DbConsecionaria.cs

--- Modelo/DbConsecionaria.cs
+++ Modelo/DbConsecionaria.cs
@@ -28,6 +28,49 @@ protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
             String connSQL = "server = DESKTOP-8BBLG0F\\SQLEXPRESS ; database=ConcesionarioV; user id=sa; password=1234";
             optionsBuilder.UseSqlServer(connSQL);
         }
+        protected override void OnModelCreating(ModelBuilder model)
+        {
+            model.Entity<Inventario>()
+                .HasOne(inventario => inventario.Distribuidoras)
+                .WithMany(distribuidora => distribuidora.Inventarios)
+                .HasForeignKey(inv => inv.DistribuidoraId);
+
+            model.Entity<RolPago>()
+              .HasOne(rolpago => rolpago.DatosClientes)
+              .WithMany(datocliente => datocliente.RolPagos)
+              .HasForeignKey(rol => rol.DatosClienteId);
+
+            model.Entity<IngresoNeto>()
+             .HasOne(ingresoneto => ingresoneto.DatosClientes)
+             .WithMany(datocliente => datocliente.IngresoNetos)
+             .HasForeignKey(ing => ing.DatosClienteId);
+
+            model.Entity<IngresoNeto>()
+               .HasOne(ingresoneto => ingresoneto.RolPagos)
+               .WithMany(rolpag => rolpag.IngresoNetos)
+               .HasForeignKey(ing => ing.RolPagoId);
+
+            model.Entity<IngresoNeto>()
+                .HasOne(ingresoneto => ingresoneto.InformeGastos)
+                .WithMany(informe => informe.IngresoNetos)
+                .HasForeignKey(ing => ing.InformeGastoId); 
+
+            model.Entity<ResumenAmortizacion>()
+                .HasOne(resumen => resumen.DatosClientes)
+                .WithMany(dato => dato.ResumenAmortizacions)
+                .HasForeignKey(res => res.DatosClienteId); 
+            model.Entity<ResumenAmortizacion>()
+                .HasOne(resumen => resumen.IngresoNetos)
+                .WithMany(ingresos => ingresos.ResumenAmortizacions)
+                .HasForeignKey(res => res.IngresoNetoId); 
+
+
+            
+            //Periodo
+
+
+
+        }
     }
     
 }
